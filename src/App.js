750d5fa0287a543df435906649c98c8694c7b702
ab105 <abidaanjum0115@gmail.com>
2021-11-04 15:52:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
+import Person from './components/Person/Person';
+
+// import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
 
+//component
+/* 
+function Countries(){
+  const[countries, setCountries] = useState([]);
+  
+  useEffect(()=>{
+    fetch('https://restcountries.com/v2/all')
+      .then(res => res.json()) //response jeta pabo shetake json convert korsi
+      .then(data => setCountries(data))//aikane setCountries a data dile useState er countries variable a jabe
+  },[]);
+  console.log(countries);
+  return(
+    <div>
+      <h2>Travelling Around the world!!!!</h2>
+      <h4>Countries Available: {countries.length}</h4>
+      {
+        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+      }
+    </div>
+  );
+}
+
+function Country(props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h2>Capital: {props.capital}</h2>
+    </div>
+  );
+}
+ */
 export default App;
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//rsc : arrow function diye component make kore ditese
+const Countries = () => {
+    return (
+        <div>
+            <h1>This is countries from its own file</h1>
+        </div>
+    );
+};
+
+export default Countries;
